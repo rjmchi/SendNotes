53ec6bef09f1e21a215627651f622137ddb832ef
rjmchi <robert@rjmprogramming.com>
2025-10-24 15:39:27 -0600

    changes logo size

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,8 +6,8 @@
             <livewire:welcome.navigation />
         @endif
         <div class="flex flex-col items-center justify-center p-6 mx-auto space-y-4 text-center max-w-7xl lg:p-8">
-            <x-app-logo />
-            <flux:button primary xl href="{{ route('register') }}">Get Started</flux:button>
+            <x-app-logo-icon class="w-30 h-30" />
+            <flux:button variant="primary" xl href="{{ route('register') }}">Get Started</flux:button>
         </div>
 
 </x-layouts.guest>
