3bdc7b0caa84a37bc952c37d16a895c60eb194b3
rjmchi <robert@rjmprogramming.com>
2025-10-23 21:53:25 -0600

    fixes id

--- database/migrations/2025_10_24_033351_create_notes_table.php
+++ database/migrations/2025_10_24_033351_create_notes_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('notes', function (Blueprint $table) {
-            $table->uuid();
+            $table->uuid('id');
             $table->string('title');
             $table->text('body');
             $table->date('send_date');
