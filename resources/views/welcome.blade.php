2efb1e7ef43df4ce2a783c0924c1df5e1c9657a7
rjmchi <robert@rjmprogramming.com>
2025-10-24 13:05:04 -0600

    welcome page changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,13 @@
 <x-layouts.guest :title="__('Welcome')">
-    <div class="flex h-full w-full flex-1 flex-col gap-4 rounded-xl">
 
-        <div>
-            SendNotes
+    <div
+        class="relative min-h-screen bg-gray-100 bg-center sm:flex sm:justify-center sm:items-center bg-dots-darker dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
+        @if (Route::has('login'))
+            <livewire:welcome.navigation />
+        @endif
+        <div class="flex flex-col items-center justify-center p-6 mx-auto space-y-4 text-center max-w-7xl lg:p-8">
+            <x-app-logo />
+            <flux:button primary xl href="{{ route('register') }}">Get Started</flux:button>
         </div>
+
 </x-layouts.guest>
